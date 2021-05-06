cd766adae46f7791ffbbf7053913bfaeea50a5e0
Dmytro Boychenko <marchiani22@gmail.com>
2021-05-06 08:32:17 -0700

    Revert "Add pie chart"
    This reverts commit f75ba998b8741f880ddb27bccbb114d49c91323b.

--- Dogomania/Dogomania/StageViewController.swift
+++ Dogomania/Dogomania/StageViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 import Charts
 
 class StageViewController: UIViewController, ChartViewDelegate {
-    var pieChart = PieChartView();
+    var lineChart = LineChartView();
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        pieChart.delegate = self
+        lineChart.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -24,23 +24,22 @@ class StageViewController: UIViewController, ChartViewDelegate {
         
         super.viewDidLayoutSubviews()
 
-        pieChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
 
-        pieChart.center = view.center
-        view.addSubview(pieChart)
+        lineChart.center = view.center
+        view.addSubview(lineChart)
         
         var entrius = [ChartDataEntry] ()
         
-        entrius.append(ChartDataEntry(x: Double(35), y: Double(35)))
-        entrius.append(ChartDataEntry(x: Double(40), y: Double(40)))
-        entrius.append(ChartDataEntry(x: Double(25), y: Double(25)))
+        for x in -5..<6 {
+            entrius.append(ChartDataEntry(x: Double(x), y: Double(x*x)))
+        }
+        let set = LineChartDataSet(entries: entrius)
+        set.colors = ChartColorTemplates.joyful()
 
-        let set = PieChartDataSet(entries: entrius)
-        set.colors = ChartColorTemplates.material()
+        let data = LineChartData(dataSet: set)
 
-        let data = PieChartData(dataSet: set)
-        #imageLiteral(resourceName: "simulator_screenshot_364D3EEB-8A8D-4D80-80C1-790F751AEFBE.png")
-        pieChart.data = data
+        lineChart.data = data
     }
     
 
